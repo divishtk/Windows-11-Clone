12b2507a18a287d99b3a26d6e57994ca6a69a907
divishtk <divisht.avion@hotmail.com>
2021-06-28 21:24:44 +0530

    Done

---
+++ srcipt.js
@@ -0,0 +1,16 @@
+const taskbar =document.getElementsByClassName("taskbar")[0];
+const startmenu=document.getElementsByClassName("startmenu")[0];
+
+taskbar.addEventListener("click",()=>{
+
+    if(startmenu.style.bottom== "50px"){
+
+        startmenu.style.bottom = "-650px"
+    }
+
+    else{
+
+        startmenu.style.bottom =  "50px" 
+    }
+
+})
